85c73a10d41a14b90fb988dff2d5c8c224907579
RickyyCoder <ricardopaul459@gmail.com>
2019-08-01 12:23:09 -0400

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,16 @@
+class StoreAdminController < ApplicationController
+
+    layout "admin"
+    #layout "order_administration", only: [:orders]
+    def home
+    end
+
+    def orders 
+        render :layout => "order_administration"
+    end
+
+    def invoice 
+        render :layout => false
+    end
+
+end
